81c5baa0ceec7061a7eb84824f3a96fc2949d579
kamiazya <yuki@kamiazya.tech>
2019-08-29 19:06:00 +0900

    add core logic about application lifecycle

---
+++ src/application.ts
@@ -0,0 +1,34 @@
+import { Injectable, InjectionToken, Injector } from 'injection-js';
+
+type LifeCycleFunction = () => Promise<void>;
+
+export const INITIALIZER = new InjectionToken<LifeCycleFunction>('application.life-cycle.initializer');
+export const TERMINATER = new InjectionToken<LifeCycleFunction>('application.life-cycle.terminater');
+
+
+@Injectable()
+export class Application {
+  constructor(private injector: Injector) {}
+
+  /**
+   * Inisializa application.
+   */
+  public async initialize(): Promise<void> {
+    return this.excute(INITIALIZER);
+  }
+
+  /**
+   * Terminate application.
+   */
+  public async terminate(): Promise<void> {
+    return this.excute(TERMINATER);
+  }
+
+  /**
+   * Execute the specified life cycle process.
+   */
+  private async excute(token: InjectionToken<LifeCycleFunction>): Promise<void> {
+    const functions = this.injector.get<LifeCycleFunction[]>(token, []);
+    await Promise.all(functions.map(f => f()));
+  }
+}
